4324f6ebda20073c527c5cb868c028154791c2be
Fahmi Abdulhamid <findfahmi@gmail.com>
2016-01-30 11:15:02 +1300

    Click button to add Patron

--- scripts/waiting_area_root.gd
+++ scripts/waiting_area_root.gd
@@ -1,14 +1,23 @@
 
 extends Node2D
 
-# member variables here, example:
-# var a=2
-# var b="textvar"
+var patronScene = null
 
 func _ready():
-	pass
+	patronScene = preload("res://scenes/patron.scn")
+	
 	
 func invite_patron():
 	print("Inviting Patron...")
-
-
+	
+	var pads = get_tree().get_nodes_in_group("pad")
+	var pad = pads[0]
+	
+	#var x = get_viewport_rect().size.width
+	var x = pad.get_pos().x
+	var y = 0
+	
+	var patronInstance = patronScene.instance()
+	patronInstance.set_pos(Vector2(x, y))
+	
+	add_child(patronInstance)
